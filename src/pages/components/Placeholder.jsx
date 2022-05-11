c7c1ec6449a78cf9de4cc08033f37968ba7dfc04
ayaz1337 <ayaz1337.burn@gmail.com>
2022-05-11 19:04:35 +0530

    Wednesday 11 May 2022 07:04:35 PM IST

--- src/pages/components/Placeholder.jsx
+++ src/pages/components/Placeholder.jsx
@@ -3,36 +3,90 @@ import React, { useState } from 'react';
 import './styles/Placeholder.scss';
 import Loader from './Loader';
 import { useEffect } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material'
+import CardImg1 from '/home/subhadip/Desktop/fimWhatever_v3.0.1/src/assets/aap-new2.png';
+import CardImg2 from '/home/subhadip/Desktop/fimWhatever_v3.0.1/src/assets/convert2.png';
+import CardImg3 from '/home/subhadip/Desktop/fimWhatever_v3.0.1/src/assets/convert.png';
+import Divider from '@mui/material/Divider';
+
+
+const items = [
+  "2016-5195",
+  "2019-19781",
+  "2018-13379"
+]
 
 export default function Placeholder() {
-  const [temp, setTemp] = useState(null)
   const [isloading, setLoading] = useState(true)
-  const [cve, setCve] = useState("CVE-2017-5196")
-  const [refresh, setRefresh] = useState(false)
+  const [data, setData] = useState({
+    "cve": "",
+    "name": "",
+    "summary": "",
+    "solutions": ""
+  })
+  const [state, setState] = useState(false)
+
 
   useEffect(() => {
-    console.log(cve)
-    axios.get(`/api/cve/${cve}`)
-    .then((response) => {
-      console.log(response.data['capec'][0])
-      setTemp(response.data['capec'][0]['summary'])
-      setLoading(false)
-      setCve(`CVE-${Math.floor(Math.random() * (2021 - 2016) + 2016)}-5196`)
+    axios.get(`/api/cve/CVE-${items[Math.floor(Math.random() * items.length)]}`)
+      .then((response) => {
+        setData({
+          ...data,
+          ["cve"]: response.data["id"],
+          ["name"]: response.data['capec'][0]['name'],
+          ["summary"]: response.data['capec'][0]['summary'],
+          ["solutions"]: response.data['capec'][0]['solutions']
+        });
+        console.log(data)
+        setLoading(false)
       })
-  }, [refresh])
+      .catch((error) => {
+        state ? setState(false) : setState(true)
+      })
+  }, [state])
 
-  const handleClick= () => {
-    refresh ? setRefresh(false) : setRefresh(true)
-  }
 
   if (isloading) {
     return <Loader />
   }
   return (
-    <div className="placeholder">
-      {cve}<br></br><br></br>
-      {temp}<br></br><br></br>
-      <button onClick={handleClick}>refresh</button>
-    </div>
+    <div className='placeholder'>
+      <Card sx={{ maxWidth: 400 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image={CardImg1}
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {data.cve}
+            </Typography>
+            <Typography gutterBottom variant="h6" component="div">
+              {data.name}
+            </Typography>
+            <Divider />
+            <Typography gutterBottom variant="h6" component="div" style={{fontSize: "18px", marginTop: "1rem"}} >
+              Summary
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {data.summary}
+            </Typography>
+            <br></br>
+            <Typography gutterBottom variant="h6" component="div" style={{fontSize: "18px"}}>
+              Solutions
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {data.solutions}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </div >
   )
 }
