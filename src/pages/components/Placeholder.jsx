b98309cc5bc101489d9e2cb95df45c19f108d245
ayaz1337 <ayaz1337.burn@gmail.com>
2022-06-13 21:23:57 +0530

    Monday 13 June 2022 09:23:57 PM IST

--- src/pages/components/Placeholder.jsx
+++ src/pages/components/Placeholder.jsx
@@ -11,22 +11,30 @@ import { CardActionArea } from '@mui/material';
 import Divider from '@mui/material/Divider';
 import Link from '@mui/material/Link';
 import { motion } from 'framer-motion';
-import CardImg1 from '../../../src/assets/convert1.png';
-import CardImg2 from '../../../src/assets/convert2.png';
-import CardImg3 from '../../../src/assets/convert3.png';
-import CardImg4 from '../../../src/assets/convert4.png';
-import CardImg5 from '../../../src/assets/convert5.png';
-import CardImg6 from '../../../src/assets/convert6.png';
-import CardImg7 from '../../../src/assets/convert7.png';
-import CardImg8 from '../../../src/assets/convert8.png';
+// import CardImg1 from '../../assets/convert1.png';
+// import CardImg2 from '../../assets/convert2.png';
+// import CardImg3 from '../../assets/convert3.png';
+// import CardImg4 from '../../assets/convert4.png';
+// import CardImg5 from '../../assets/convert5.png';
+// import CardImg6 from '../../assets/convert6.png';
+// import CardImg7 from '../../assets/convert7.png';
+// import CardImg8 from '../../assets/convert8.png';
 
 
-const img = [CardImg1, CardImg2, CardImg3, CardImg4, CardImg5, CardImg6, CardImg7, CardImg8]
+const img = [
+  'https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https:/cdn.filestackcontent.com/E36hbutZSviV09P3jLn3',
+  'https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https:/file-uploads.teachablecdn.com/ca913777c7834117921514f999bce5f1/987a11780f304270b1ff713ceed7517e',
+  'https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https:/file-uploads.teachablecdn.com/956925ec734a46efbb0ac43c562eeff1/5d0949b474054798bee6d56f5e19b64d',
+  'https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https:/file-uploads.teachablecdn.com/cea96d685f35459ca89c69b7212687f1/2d37f6d87efc454499a71894d3e762b0',
+  'https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https:/file-uploads.teachablecdn.com/a748e3ca7d024358970ad77d2c73f3f1/296a2b34f81544bfbbb1c4cf043c0db8',
+  'https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https:/file-uploads.teachablecdn.com/a625eecc19d74664a41728c4047497f1/f017cfd1161b4a0ca5dc8fb5a54219ed',
+  'https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https:/file-uploads.teachablecdn.com/5b4e4571eb7c47c89be66aa7072598f1/9d2f1c4df3ee4596b027badc6f723b5b'
+]
 
 export default function Placeholder() {
   const [isloading, setLoading] = useState(true)
   const [data, setData] = useState({
-    "img": "CardImg1",
+    "img": "https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https:/file-uploads.teachablecdn.com/5b4e4571eb7c47c89be66aa7072598f1/9d2f1c4df3ee4596b027badc6f723b5b",
     "cve": "2016-5195",
     "name": "Leveraging Race Conditions",
     "summary": "The adversary targets a race condition occurring when multiple processes access and manipulate the same resource concurrently, and the outcome of the execution depends on the particular order in which the access takes place. The adversary can leverage a race condition by \"running the race\", modifying the resource and modifying the normal execution flow. For instance, a race condition can occur while accessing a file: the adversary can trick the system by replacing the original file with his version and cause the system to read the malicious file.",
@@ -58,7 +66,7 @@ export default function Placeholder() {
   if (isloading) {
     return <Loader />
   }
-  
+
   return (
     <motion.div
       initial={{ opacity: 0, scale: 0.5 }}
